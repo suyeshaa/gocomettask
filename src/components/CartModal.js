e97518ead04c36834708a290354b2a9766c15f2d
Suyesha Chaudhary <suyeshaachaudhry@gmail.com>
2023-08-25 22:15:10 +0530

    add cart

--- src/components/CartModal.js
+++ src/components/CartModal.js
@@ -4,6 +4,22 @@ import { RiCloseLine } from "react-icons/ri";
 import { productData } from "../data";
 
 const CartModal = ({ setIsOpen }) => {
+  let totalPrice;
+  let actPrice;
+  let ogPriceArr = productData.map((it) => it.originalPrice);
+  let actPriceArr = productData.map((it) => it.price);
+
+  totalPrice = ogPriceArr.reduce(
+    (prevValue, currValue) => prevValue + currValue
+  );
+  actPrice = actPriceArr.reduce(
+    (prevValue, currValue) => prevValue + currValue
+  );
+
+  let discount = totalPrice - actPrice;
+
+  // console.log(totalPrice);
+
   return (
     <div>
       <div className="bg" onClick={() => setIsOpen(false)} />
@@ -40,7 +56,35 @@ const CartModal = ({ setIsOpen }) => {
                 );
               })}
             </div>
-            <div className="summary heading">Summary</div>
+            <div className="summary heading">
+              Summary
+              <div className="cart-info">
+                <div className="checkout-info price-heading">
+                  PRICE DETAILS ({productData.length} items)
+                </div>
+                <div className="checkout-info">
+                  <div>Total MRP</div>
+                  <div className="totPrice">Rs.{totalPrice}</div>
+                </div>
+                <div className="checkout-info">
+                  <div>Discount on MRP</div>
+                  <div className="totDisc">-Rs.{discount}</div>
+                </div>
+                <div className="checkout-info">
+                  <div>Convenience Fee</div>
+                  <div className="conven">
+                    <span>Rs.99</span> Rs.15
+                  </div>
+                </div>
+                <div className="price-info checkout-info">
+                  <div>Total Amount</div>
+                  <div>Rs.{actPrice}</div>
+                </div>
+                <div>
+                  <button className="order-btn">PLACE ORDER</button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
